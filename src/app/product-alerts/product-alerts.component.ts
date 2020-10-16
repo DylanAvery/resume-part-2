74a8a2b4717473d2cace019ddf7a10ad335a1b53
DylanAvery <dylavery712@gmail.com>
2020-10-16 11:11:08 -0400

    Section 2 - Input complete

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
 
+ @Input() product;
   constructor() { }
 
   ngOnInit() {
